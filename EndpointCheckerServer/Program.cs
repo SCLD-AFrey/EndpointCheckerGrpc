ad3c74e0b19b5bb5f697112eeacfbc082eea8695
Arthur Frey <afrey@steelcloud.com>
2020-04-24 11:52:09 -0400

    added database

--- EndpointCheckerServer/Program.cs
+++ EndpointCheckerServer/Program.cs
@@ -1,31 +1,79 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Grpc.Core;
 using EndpointChecker;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using Microsoft.Extensions.Configuration;
 
 namespace EndpointCheckerServer
 {
     class EndpointCheckerImpl : EndpointChecker.EndpointChecker.EndpointCheckerBase
     {
 
+
         public override Task<EndpointReply> CheckEndpoint(EndpointRequest request, ServerCallContext context)
         {
-            var msg = string.Format("Checking {0} @ {1} : ", request.Name, request.IPaddress);
-            var success = true;
-            if (request.Platform.ToLower() == "windows")
+            var lErrors = new List<string>();
+            var reply = new EndpointReply
             {
-                msg += " Server Passed";
+                StartTime = DateTime.Now.ToString(), 
+                Success = true
+            };
 
-            }
-            else
+
+            if (request.Platform.ToLower() != "windows")
             {
-                msg += " Server is not a windows server";
-                success = false;
+                reply.Success = false;
+                lErrors.Add(request.Platform + " not allowed");
             }
-            return Task.FromResult(new EndpointReply { Message = msg, Success = success });
+
+            if (!request.IPaddress.StartsWith("10."))
+            {
+                reply.Success = false;
+                lErrors.Add(request.IPaddress + " not within IP range of 10.*.*.*");
+            }
+
+
+
+            InsertCheck(request, reply);
+            reply.Message = String.Format("Server {0} at {1} returned a Success of {2}",request.Name,request.IPaddress,reply.Success.ToString());
+            if (!reply.Success)
+            {
+                reply.Error = string.Join(",", lErrors);
+                reply.Message += " due to " + reply.Error;
+            }
+
+            return Task.FromResult(reply);
+        }
+
+        public void InsertCheck(EndpointRequest request, EndpointReply reply)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                reply.EndTime = DateTime.Now.ToString();
+
+                TimeSpan tranSpan = DateTime.Parse(reply.EndTime) - DateTime.Parse(reply.StartTime);
+
+
+                reply.TransactionTime = tranSpan.Milliseconds;
+                EndpointCheck check = new EndpointCheck(unitOfWork)
+                {
+                    Name = request.Name,
+                    IPaddress = request.IPaddress,
+                    Platform = request.Platform,
+                    Success = reply.Success,
+                    Error = reply.Error,
+                    StartTime = DateTime.Parse(reply.StartTime),
+                    EndTime = DateTime.Parse(reply.EndTime)
+                };
+                unitOfWork.CommitChanges();
+            }
         }
 
     }
+
 
     class Program
     {
@@ -33,6 +81,11 @@ namespace EndpointCheckerServer
 
         public static void Main(string[] args)
         {
+            IConfiguration AppConfiguration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            ConnectionHelper.Connect(AppConfiguration, autoCreateOption: AutoCreateOption.DatabaseAndSchema);
+
             Server server = new Server
             {
                 Services = { EndpointChecker.EndpointChecker.BindService(new EndpointCheckerImpl()) },
